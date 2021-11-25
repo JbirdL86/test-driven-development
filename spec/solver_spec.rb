78f1c095b123eda3cf6610fe76e637b0d9fb90d1
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-25 12:50:06 -0500

    Add tests with tdd methodology

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,13 +11,21 @@ describe Solver do
       expect(new_solver.factorial(4)).to eq 24
     end
   end
-
   context 'When testing the Solver reverse method' do
     it 'Should return a reverse string' do
       new_solver = Solver.new
       expect(new_solver.reverse('hello')).to eq 'olleh'
       expect(new_solver.reverse('juanito')).to eq 'otinauj'
       expect(new_solver.reverse('google')).to eq 'elgoog'
+    end
+  end
+  context 'When testing the fizzbuzz method' do 
+    it 'Should return a string' do
+      new_solver = Solver.new
+      expect(new_solver.fizzbuzz(15)).to eq "fizzbuzz"
+      expect(new_solver.fizzbuzz(9)).to eq "fizz"
+      expect(new_solver.fizzbuzz(10)).to eq "buzz"
+      expect(new_solver.fizzbuzz(4)).to eq "Its not divisble by 3 nor 5"
     end
   end
 end
