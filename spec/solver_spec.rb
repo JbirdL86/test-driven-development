67f98eae329e7d5f3f1dcf76aa5e9e8248c1df50
JbirdL86 <jgonzalez.wj@gmail.com>
2021-11-25 11:15:28 -0600

    Code Sover class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,10 @@
+require_relative '../solver'
+
 describe Solver do
   context 'When testing the Solver class' do
     it 'Should create a new solver when we call the class method new' do
       new_solver = Solver.new
-      expect(new_solver.class).to eq 'Solver'
+      expect(new_solver.class.to_s).to eq 'Solver'
     end
   end
 end
