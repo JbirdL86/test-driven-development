1fd366716a12bc48bf3fa968b4860cfb5fbdd147
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-25 12:50:49 -0500

    Add solver functions

--- solver.rb
+++ solver.rb
@@ -16,5 +16,17 @@ class Solver
 
   def reverse(string)
     string.reverse!
+  end 
+  
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      return "fizzbuzz"
+    elsif n % 3 == 0
+      return "fizz"
+    elsif n % 5 == 0
+      return "buzz"
+    else 
+      return "Its not divisble by 3 nor 5"
+    end
   end
 end
